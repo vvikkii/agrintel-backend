3bd09cf8af55cf8150a8d8041525547d4282e293
Vikrant Singh <154698356+vvikkii@users.noreply.github.com>
2026-05-08 03:09:44 +0530

    updated main.py

--- main.py
+++ main.py
@@ -5,18 +5,30 @@ import joblib
 
 app = FastAPI()
 
-# Load model
+# Load trained model
 model = joblib.load("fertilizer_model.pkl")
 
 # Input schema
 class InputData(BaseModel):
-    Nitrogen: float
-    Phosphorus: float
-    Potassium: float
-    pH: float
-    Moisture: float
+    Soil_Type: int
+    Soil_pH: float
+    Soil_Moisture: float
+    Organic_Carbon: float
+    Electrical_Conductivity: float
+    Nitrogen_Level: float
+    Phosphorus_Level: float
+    Potassium_Level: float
     Temperature: float
     Humidity: float
+    Rainfall: float
+    Crop_Type: int
+    Crop_Growth_Stage: int
+    Season: int
+    Irrigation_Type: int
+    Previous_Crop: int
+    Region: int
+    Fertilizer_Used_Last_Season: int
+    Yield_Last_Season: float
 
 @app.get("/")
 def home():
@@ -25,19 +37,29 @@ def home():
 @app.post("/predict")
 def predict(data: InputData):
 
-    input_dict = {
-        "Nitrogen": [data.Nitrogen],
-        "Phosphorus": [data.Phosphorus],
-        "Potassium": [data.Potassium],
-        "pH": [data.pH],
-        "Moisture": [data.Moisture],
-        "Temperature": [data.Temperature],
-        "Humidity": [data.Humidity]
-    }
+    input_data = pd.DataFrame([{
+        "Soil_Type": data.Soil_Type,
+        "Soil_pH": data.Soil_pH,
+        "Soil_Moisture": data.Soil_Moisture,
+        "Organic_Carbon": data.Organic_Carbon,
+        "Electrical_Conductivity": data.Electrical_Conductivity,
+        "Nitrogen_Level": data.Nitrogen_Level,
+        "Phosphorus_Level": data.Phosphorus_Level,
+        "Potassium_Level": data.Potassium_Level,
+        "Temperature": data.Temperature,
+        "Humidity": data.Humidity,
+        "Rainfall": data.Rainfall,
+        "Crop_Type": data.Crop_Type,
+        "Crop_Growth_Stage": data.Crop_Growth_Stage,
+        "Season": data.Season,
+        "Irrigation_Type": data.Irrigation_Type,
+        "Previous_Crop": data.Previous_Crop,
+        "Region": data.Region,
+        "Fertilizer_Used_Last_Season": data.Fertilizer_Used_Last_Season,
+        "Yield_Last_Season": data.Yield_Last_Season
+    }])
 
-    df = pd.DataFrame(input_dict)
-
-    prediction = model.predict(df)
+    prediction = model.predict(input_data)
 
     return {
         "recommended_fertilizer": str(prediction[0])
